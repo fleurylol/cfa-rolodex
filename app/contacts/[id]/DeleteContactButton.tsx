075d27cacb0a390826704f1276a22a6327202701
zfleury <zach@fleury.lol>
2024-01-26 10:40:47 -0600

    delete confirm

--- app/contacts/[id]/DeleteContactButton.tsx
+++ app/contacts/[id]/DeleteContactButton.tsx
@@ -1,11 +1,42 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Flex,
+} from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
 
 const DeleteContactButton = ({ contactId }: { contactId: number }) => {
   return (
-    <Button>
-      <TrashIcon /> Delete Contact
-    </Button>
+    <AlertDialogRoot>
+      <AlertDialogTrigger>
+        <Button>
+          <TrashIcon /> Delete Contact
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+        <AlertDialogDescription>
+          Are you sure you want to delete this contact? This cannot be undone!
+        </AlertDialogDescription>
+        <Flex mt="4" gap={"3"}>
+          <AlertDialogCancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <AlertDialogAction>
+            <Button>Delete Contact</Button>
+          </AlertDialogAction>
+        </Flex>
+      </AlertDialogContent>
+    </AlertDialogRoot>
   );
 };
 
