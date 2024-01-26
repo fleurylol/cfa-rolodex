dfe680a424765cd9aa698ba66b10391553f43532
zfleury <zach@fleury.lol>
2024-01-26 11:00:12 -0600

    error handles

--- app/contacts/[id]/DeleteContactButton.tsx
+++ app/contacts/[id]/DeleteContactButton.tsx
@@ -13,41 +13,62 @@ import {
 import { TrashIcon } from "@radix-ui/react-icons";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteContactButton = ({ contactId }: { contactId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+  const deleteContact = async () => {
+    try {
+      await axios.delete("/api/contacts/" + contactId);
+      router.push("/contacts");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
-    <AlertDialogRoot>
-      <AlertDialogTrigger>
-        <Button>
-          <TrashIcon /> Delete Contact
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
-        <AlertDialogDescription>
-          Are you sure you want to delete this contact? This cannot be undone!
-        </AlertDialogDescription>
-        <Flex mt="4" gap={"3"}>
-          <AlertDialogCancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialogCancel>
-          <AlertDialogAction>
-            <Button
-              onClick={async () => {
-                await axios.delete("/api/contacts/" + contactId);
-                router.push("/contacts");
-                router.refresh();
-              }}
-            >
-              Delete Contact
-            </Button>
-          </AlertDialogAction>
-        </Flex>
-      </AlertDialogContent>
-    </AlertDialogRoot>
+    <>
+      <AlertDialogRoot>
+        <AlertDialogTrigger>
+          <Button>
+            <TrashIcon /> Delete Contact
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Confirm Deletion</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete this contact? This cannot be undone!
+          </AlertDialogDescription>
+          <Flex mt="4" gap={"3"}>
+            <AlertDialogCancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialogCancel>
+            <AlertDialogAction>
+              <Button onClick={deleteContact}>Delete Contact</Button>
+            </AlertDialogAction>
+          </Flex>
+        </AlertDialogContent>
+      </AlertDialogRoot>
+      <AlertDialogRoot open={error}>
+        <AlertDialogContent>
+          <AlertDialogTitle>Error</AlertDialogTitle>
+          <AlertDialogDescription>
+            Aw man something isnt working
+          </AlertDialogDescription>
+          <Button
+            variant="soft"
+            color="gray"
+            mt={"2"}
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialogContent>
+      </AlertDialogRoot>
+    </>
   );
 };
 
