db2ea1e6daa15c4aff0a7c51fe3dea164dc735f7
zfleury <zach@fleury.lol>
2024-02-08 16:21:54 -0600

    comment form render only when signed in

--- app/contacts/[id]/CommentSection.tsx
+++ app/contacts/[id]/CommentSection.tsx
@@ -1,23 +1,25 @@
+"use client";
 import CommentForm from "@/app/comments/CommentForm";
 import { Contact } from "@prisma/client";
-import { Box, Card, Text } from "@radix-ui/themes";
+import { Box, Card, Link, Skeleton, Text } from "@radix-ui/themes";
 import React from "react";
 import CommentList from "./CommentList";
+import { useSession } from "next-auth/react";
 
 const CommentSection = ({ contact }: { contact: Contact }) => {
+  const { data: session } = useSession();
   return (
     <>
       <Box>
-        <Card>
-          <Text>Add comments for {contact.name}</Text>
-        </Card>
+        <Text>Notes for {contact.name}</Text>
       </Box>
-      <Box>
-        <CommentForm contact={contact} />
-      </Box> 
+      {session && (
+        <Box>
+          <CommentForm contact={contact} />
+        </Box>
+      )}
       <Box>
         <Card>
-          <Text>Comments</Text>
           <CommentList contact={contact} />
         </Card>
       </Box>
