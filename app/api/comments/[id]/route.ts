4626d9956ff9f07e2329dd82ef5593599c86fe0a
zfleury <zach@fleury.lol>
2024-02-07 18:24:07 -0600

    removed get api auth for comments

--- app/api/comments/[id]/route.ts
+++ app/api/comments/[id]/route.ts
@@ -1,14 +1,10 @@
-import authOptions from "@/app/auth/authOptions";
 import prisma from "@/prisma/client";
-import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
   const contact = await prisma.contact.findUnique({
     where: { id: parseInt(params.id) },
   });
