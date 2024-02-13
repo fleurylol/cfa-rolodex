66c966f9e6f9cfeda033f4a5b81e5ef133452b8c
zfleury <zach@fleury.lol>
2024-02-13 14:09:20 -0600

    business page adustments

--- app/business/[id]/page.tsx
+++ app/business/[id]/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Heading, Text, Box } from "@radix-ui/themes";
+import { Heading, Text, Box, Button, Flex, Grid } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import ContactList from "./ContactList";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 interface Props {
   params: { id: string };
 }
 
 const BusinessDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
+
   const business = await prisma.business.findUnique({
     where: { id: parseInt(params.id) },
   });
@@ -16,12 +20,25 @@ const BusinessDetailPage = async ({ params }: Props) => {
   const { name, address } = business;
   return (
     <>
-      <Box>
-        <Heading>{name}</Heading>
-        <Text>{address}</Text>
-      </Box>
-      <Text>Contacts: </Text>
-      <ContactList business={business}/>
+      <Grid columns={{ initial: "1", sm: "5" }}>
+        <Box className="md:col-span-4">
+          <Flex direction={"column"} className="mb-3">
+            <Heading>{name}</Heading>
+            <Text>{address}</Text>
+          </Flex>
+
+          <Text>Contacts: </Text>
+          <ContactList business={business} />
+        </Box>
+        {session && (
+          <Box>
+            <Flex direction="column" gap="4">
+              <Button></Button>
+              <Button></Button>
+            </Flex>
+          </Box>
+        )}
+      </Grid>
     </>
   );
 };
