8eed00f19047b6a353c49327586c7741bebba5d9
zfleury <zach@fleury.lol>
2024-02-22 17:29:08 -0600

    no changes

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -17,7 +17,7 @@ import {
 
 const NavBar = () => {
   return (
-    <nav className="border-b mb-5 px-5 py-3 bg-red-600">
+    <nav className="mb-5 border-b bg-red-600 px-5 py-3">
       <Container>
         <Flex justify="between">
           <Flex align="center" gap={"5"}>
