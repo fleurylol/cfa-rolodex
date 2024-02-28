d0d3f19073e507e9dd1a685e18503c032f283c62
zfleury <zach@fleury.lol>
2024-02-28 14:15:05 -0600

    page displaying and linking

--- app/home/_components/PageBox.tsx
+++ app/home/_components/PageBox.tsx
@@ -1,30 +1,46 @@
-"use client";
 import { Box, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+interface PageBoxProps {
+  link: string;
+  img: string; //src={"/images/CateringComp.png"}
+  name: string;
+}
 
-
-const PageBox = () => {
+const PageBox: React.FC<PageBoxProps> = ({ link, img, name }) => {
   return (
-    <Link href="https://www.radix-ui.com/themes/docs/components/card">
+    <Link href={link}>
       <Box className="max-w-auto rounded-md border">
         <Box className="m-5 flex flex-col items-center justify-center">
           <div className="mb-5 border">
             <Image
-              src={"/images/CateringComp.png"}
+              src={img}
               alt=""
               width={332}
               height={332}
               loading={"lazy"}
             ></Image>
           </div>
-          <Text>Catering Compantion</Text>
+          <Text>{name}</Text>
         </Box>
       </Box>
     </Link>
   );
 };
+
+export const pages = [
+  {
+    link: "https://fleurylol.github.io/CateringOCR/",
+    img: "/images/CateringComp.png",
+    name: "Catering Companion",
+  },
+  {
+    link: "https://fleurylol.github.io/CherryValeMap/",
+    img: "/images/MallLogo.png",
+    name: "CherryVale Mall Parking Map",
+  },
+];
 
 export default PageBox;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 import { Metadata } from "next";
 import Header from "./home/Header";
 import { Grid } from "@radix-ui/themes";
-import PageBox from "./home/_components/PageBox";
+import PageBox, { pages } from "./home/_components/PageBox";
 
 export default async function Home() {
+  const appPages = pages;
+  interface Page {
+    link: string;
+    img: string;
+    name: string;
+  }
   return (
     <>
       <Header />
-      <Grid columns="1" gap="3" width="auto">
-        <PageBox />
-        <PageBox />
-        <PageBox />
+      <Grid columns={{ initial: "1", md: "3" }} gap="3" width="auto">
+        {appPages.map((page: Page) => (
+          <PageBox link={page.link} img={page.img} name={page.name} />
+        ))}
       </Grid>
     </>
   );
