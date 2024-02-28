cccc74f84c9f1365d52edb779545e0b2b574954c
zfleury <zach@fleury.lol>
2024-02-28 14:43:12 -0600

    fixed missing key

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,12 @@ export default async function Home() {
       <Header />
       <Grid columns={{ initial: "1", md: "3" }} gap="3" width="auto">
         {appPages.map((page: Page) => (
-          <PageBox link={page.link} img={page.img} name={page.name} />
+          <PageBox
+            key={page.link}
+            link={page.link}
+            img={page.img}
+            name={page.name}
+          />
         ))}
       </Grid>
     </>
@@ -27,4 +32,4 @@ export const metadata: Metadata = {
   description: "Home page",
 };
 
-// https://docs.google.com/forms/d/e/1FAIpQLSeFImfvQieyhgCrj63KBx-AwgoylRME3K6hUA4S_TYKHchNhw/viewform
+// x
